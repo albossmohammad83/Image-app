6230ef490620ec650554e7dd57b089a400d883be
Mohammad Abdelrahman <albossmohammad83@gmail.com>
2020-11-21 11:01:12 -0800

    Adding all html from assignments 2,3,4 and change it to .hbs and adding a home page with header and footer

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+  res.render('index',{title:"instructor APP"});
+});
+
+router.get('/login',(req, res, next) => {
+  res.render("login",{title:"Log IN"})
+});
+
+router.get('/registration',(req, res, next) => {
+  res.render("registration",{title:"Registration"})
+});
+
+router.get('/postimage',(req, res, next) => {
+  res.render("postimage",{title:"Post Image"})
+});
+
+router.get('/imagepost',(req, res, next) => {
+  res.render("imagepost",{title:"Image Post"})
 });
 
 module.exports = router;
